36e9a4df93fe56df9480861829998d368e3166e2
sanchez-christian <sanchezchristian.us@gmail.com>
2022-03-30 09:57:04 -0700

    End 3/30/22

---
+++ cancerdata.py
@@ -0,0 +1,16 @@
+
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/")
+def render_main():
+    return render_template('index.html')
+
+@app.route("/p1")
+def render_page1():
+    return render_template('data.html')
+    
+if __name__=="__main__":
+    app.run(debug=False)
+
