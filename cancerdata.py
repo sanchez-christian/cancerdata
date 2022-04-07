7a50b198a249a3ac9763a66e11467d1dff9fefc1
sanchez-christian <sanchezchristian.us@gmail.com>
2022-04-07 10:21:47 -0700

    End 4/7/22

--- cancerdata.py
+++ cancerdata.py
@@ -13,24 +13,34 @@ def render_page1():
 
 @app.route("/rates")
 def render_page2():
-    return render_template('rates.html')
+    return render_template('rates.html', rates = format_dict_as_graph_points2())
     
 
 
 def format_dict_as_graph_points():
     with open('cancer.json') as numbers_data:
-        data = json.load(numbers_data)
-    graph_points = ""
+        num = json.load(numbers_data)
+    graph_numbers = ""
 #    state = request.args['State']
 #    num = 0
-    for s in data:
+    for s in num:
 #        if s["State"] == state:
 #            num = s["Numbers"]
         #{ label: "India", y: 7.1 },
-        graph_points = graph_points + Markup('{label: "' + s["State"] + '" , y: ' + str(s["Total"]["Number"]) + '},' )
-    graph_points = graph_points[:-1] #this will remove the last comma and space
-    print(graph_points)
-    return graph_points #will take it and send it back to line 12
+        graph_numbers = graph_numbers + Markup('{label: "' + s["State"] + '" , y: ' + str(s["Total"]["Number"]) + '},' )
+    graph_numbers = graph_numbers[:-1] #this will remove the last comma and space
+    print(graph_numbers)
+    return graph_numbers #will take it and send it back to line 12
+
+def format_dict_as_graph_points2():
+    with open('cancer.json') as rates_data:
+        rate = json.load(rates_data)
+    graph_rates = ""
+    for r in rate:
+        graph_rates = graph_rates + Markup('{label: "' + r["State"] + '" , y: ' + str(r["Total"]["Rate"]) + '},' )
+    graph_rates = graph_rates[:-1] #this will remove the last comma and space
+    print(graph_rates)
+    return graph_rates #will take it and send it back to line 12
 
 if __name__=="__main__":
     app.run(debug=True)
